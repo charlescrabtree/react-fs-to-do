ceb876925d5a86a7869cbeb8a45298a8e0895c9e
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-10-13 19:50:01 -0700

    meant to do this..

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,43 +1,44 @@
-// import { useContext, useState } from 'react';
-// import { Redirect, useParams } from 'react-router-dom';
-// import { UserContext } from '../../context/UserContext';
-// import { authUser } from '../../services/auth';
-// import './Auth.css';
+import { useContext, useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+import { authUser } from '../../services/auth';
+import './Auth.css';
 
-// export default function Auth() {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const { type } = useParams();
+export default function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { type } = useParams();
 
-//   const { user, setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
-//   const useAuth = async () => {
-//     const userResp = await authUser(email, password, type);
-//     setUser(userResp);
-//     setEmail('');
-//     setPassword('');
-//   };
+  const useAuth = async () => {
+    const userResp = await authUser(email, password, type);
+    setUser(userResp);
+    setEmail('');
+    setPassword('');
+  };
   
-//   if (user) return <Redirect to='/todos' />;
+  if (user) return <Redirect to='/todos' />;
 
-//   return (
-//     <div className='container'>
-//       <div>
-//         <label className='label'>Email</label>
+  return (
+    <div></div>
+    // <div className='container'>
+    //   <div>
+    //     <label className='label'>Email</label>
         
-//         <input className='input' type='text' placeholder='email@email.com' value={email} onChange={(e) => {
-//           setEmail(e.target.value);
-//         }} />
+    //     <input className='input' type='text' placeholder='email@email.com' value={email} onChange={(e) => {
+    //       setEmail(e.target.value);
+    //     }} />
         
-//         <div>
-//           <label className='label'>Password</label>
-//           <input className='password' type='text' placeholder='Password please' value={password} onChange={(e) => {
-//             setPassword(e.target.value);
-//           }} />
-//         </div>
-//         <button className='button' onClick={useAuth}>Submit</button>
-//       </div>
-//     </div>
+    //     <div>
+    //       <label className='label'>Password</label>
+    //       <input className='password' type='text' placeholder='Password please' value={password} onChange={(e) => {
+    //         setPassword(e.target.value);
+    //       }} />
+    //     </div>
+    //     <button className='button' onClick={useAuth}>Submit</button>
+    //   </div>
+    // </div>
 
-//   );
-// }
+  );
+}
